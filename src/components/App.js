2c7bf1c38c47d73a6201e721815dc671dd8ba21c
imrahulsrivastava <imrahulsrivastava1@gmail.com>
2024-03-21 13:47:02 +0530

    Submission

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <p>I am learning React. My life is getting better.</p>;
+  return (
+    <main>
+      <p>I am learning React. My life is getting better.</p>
+    </main>
+  );
 }
 
 export default App;
